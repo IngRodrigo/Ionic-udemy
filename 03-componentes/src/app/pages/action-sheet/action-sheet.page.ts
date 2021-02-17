7743e3a2a74d563aae017e2b5f7e53d3ebbe323a
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2021-02-17 17:00:53 -0300

    continuando componentes basicos

--- 03-componentes/src/app/pages/action-sheet/action-sheet.page.ts
+++ 03-componentes/src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
+
+  
+  //se debe inyectar al constructor de la clase el servicio ActionSheetController
+  constructor(private actionSheetController:ActionSheetController) { }
 
   ngOnInit() {
+    
   }
+
+  onClick(){
+    this.presentActionSheet();
+  }
+
+  //documentacion: https://ionicframework.com/docs/api/action-sheet
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Mis albunes',
+      backdropDismiss:false,
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        cssClass: 'rojo',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 
 }
